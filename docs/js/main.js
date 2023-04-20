e2892ea72fcce69459f4cc19664951d313129ddd
Dih42 <123738453+Dih42@users.noreply.github.com>
2023-04-20 20:20:01 +0500

    Delete docs directory

--- docs/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.directions__filter-item').on('click', function(){
-        $('.directions__filter-item').removeClass('directions__filter-item--active')
-        $(this).addClass('directions__filter-item--active')
-    })
-})
